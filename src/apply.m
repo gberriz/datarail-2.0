ecea16415c3e2ec0e28f9821b1122cecf6ba8724
Gabriel F. Berriz <gabriel_berriz@hms.harvard.edu>
2013-05-16 18:28:27 -0400

    extend apply to nominals

--- src/apply.m
+++ src/apply.m
@@ -1,11 +1,9 @@
 function dset = apply(fn, dset, varname)
-  try
-    dset.(varname) = cellmap(fn, dset.(varname));
-  catch exc
-    if not(strcmp(exc.identifier, 'MATLAB:cellfun:NotACell') && ...
-           strcmp(exc.message, ['Input #2 expected to be a cell array, ' ...
-                                'was double instead.']))
-      rethrow(exc)
-    end
-    dset.(varname) = arraymap(fn, dset.(varname));
+  var = dset.(varname);
+  if isa(var, 'nominal')
+    dset.(varname) = nominal(cellmap(fn, cellstr(var)));
+  elseif isa(var, 'cell')
+    dset.(varname) = cellmap(fn, var);
+  else
+    dset.(varname) = arraymap(fn, var);
   end
