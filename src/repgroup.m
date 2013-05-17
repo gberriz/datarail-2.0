81adcf7ddf9656a20b9a0bb08b5c984428b6fdaf
Gabriel F. Berriz <gabriel_berriz@hms.harvard.edu>
2013-05-17 13:06:20 -0400

    fix bugs

--- src/repgroup.m
+++ src/repgroup.m
@@ -1,10 +1,15 @@
 function out = repgroup(dset, keycols)
 sep = native2unicode(28);
-n = length(dset);
+subds = dset(:, keycols);
+clear('dset');
+n = length(subds);
 c = zeros(n);
 seen = containers.Map();
 for i = 1:n
-  k = dataset2cell(dset(i, keycols));
-  k = strjoin(k(2, :), sep);
-  c(i) = seen(k) = mapget(seen, k, -1) + 1;
+  k = dataset2cell(subds(i, :));
+  k = strjoin(cellmap(@num2str, k(2, :)), sep);
+  r = mapget(seen, k, -1) + 1;
+  c(i) = r;
+  seen(k) = r;
 end
+out = arraymap(@num2str, c);
