bf23f23a73a9aeaab8efedb3053498db57f7ac44
Gabriel F. Berriz <gabriel_berriz@hms.harvard.edu>
2013-05-20 13:59:29 -0400

    generalize datapath

--- src/mgh1.m
+++ src/mgh1.m
@@ -1,9 +1,8 @@
 % -*- mode: matlab -*-
 basename = 'bl1';
 basename = 'BreastLinesFirstBatch_MGHData_sent';
-datapath = fullfile('/Users/berriz/Work/scratch/DR20/data', ...
+datapath = fullfile(mfiledir(), '..', 'data', ...
                     sprintf('%s.xlsx', basename));
-% datapath = fullfile('..', 'data', sprintf('%s.xlsx', basename));
 warning('off', 'MATLAB:codetools:ModifiedVarnames');
 welldata = dataset('XLSFile', datapath, 'Sheet', 'WellDataMapped');
 
