2d806a248d82383d195106527001d20e44c2ceb3
Gabriel F. Berriz <gabriel_berriz@hms.harvard.edu>
2013-05-30 13:40:53 -0400

    repgroup: speed up + arg check

--- src/repgroup.m
+++ src/repgroup.m
@@ -1,15 +1,40 @@
 function out = repgroup(dset, keycols)
+
+sub = dset(:, keycols);
+clear dset;
+
+% assumes that the sub-dataset of DSET specified by the varnames in
+% KEYCOLS must consist entirely of strings.
+
+cls = datasetmap(@(x) ...
+    unique(cellmap(@class, x)), sub); cls = unique(cat(1, cls{:}));
+
+if ~(length(cls) == 1 && isequal(cls{1}, 'char'))
+  error('DR20:regroup:NotAllChar', ...
+        'Not all values in KEYCOLS columns are of type ''char''');
+end
+clear cls;
+
+sub = dataset2cell(sub); sub = sub(2:end, :);
+n = length(sub);
+
+keys = cell([n 1]);
 sep = native2unicode(28);
-subds = dset(:, keycols);
-clear('dset');
-n = length(subds);
-c = zeros(n, 1, 'uint8');
-seen = containers.Map();
 for i = 1:n
-  k = dataset2cell(subds(i, :));
-  k = strjoin(cellmap(@num2str, k(2, :)), sep);
-  r = mapget(seen, k, -1) + 1;
+  keys{i} = CStr2String(sub(i), sep, false);
+end
+clear sub;
+
+ukeys = unique(keys);
+seen = containers.Map(ukeys, zeros(length(ukeys), 1, 'uint8') - 1);
+clear ukeys;
+
+c = zeros(n, 1, 'uint8');
+for i = 1:n
+  k = keys{i};
+  r = seen(k) + 1;
   c(i) = r;
   seen(k) = r;
 end
-out = arraymap(@num2str, c);
+
+out = arraymap(@(x) sprintf('%d', x), c);
